dba773bdbe844b8dc6669bd7015db52e4aa30713
Young Foo <fuymsn@hotmail.com>
2015-10-20 22:39:15 +0800

    add management page

---
+++ backend.php
@@ -0,0 +1,53 @@
+<?php
+    require("./state.php");
+    var_dump($_SESSION);
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>backend</title>
+    <link rel="stylesheet" href="lib/bootstrap/css/bootstrap.min.css">
+    <script src="lib/jquery-2.0.1.min.js"></script>
+    <script src="lib/bootstrap/js/bootstrap.min.js"></script>
+    <style>
+      .main-message{ padding: 30px 15px 5px 15px;}
+      .control-label{ line-height: 34px;}
+      iframe{ border: none;}
+      .kbn-timepicker-section{
+        float: left;
+        margin-right: 40px;
+      }
+    </style>
+</head>
+<body>
+  <div class="container">
+      <nav class="navbar navbar-default" role="navigation">
+        <div class="container-fluid">
+          <div class="navbar-header">
+            <a class="navbar-brand" href="#">金分</a>
+          </div>
+          <ul class="nav navbar-nav navbar-right">
+            <li><a href="#"><?php echo $_SESSION["email"] ?></a></li>
+            <li><a href="./loginout.php">退出</a></li>
+          </ul>
+        </div>
+      </nav>
+    <div class="row">
+        <div class="col-md-3">
+          <ul class="list-group">
+            <li class="list-group-item">添加类别</li>
+          </ul>
+        </div>
+        <div class="col-md-9">
+          <div class="panel panel-default">
+            <div class="panel-body">
+				container
+            </div>
+          </div>
+        </div>
+    </div>
+  </div>
+</body>
+</html>
