9ee1aa0965a0cef76cadc4843b47d559b8e0b454
Young Foo <mr.f.young@gmail.com>
2015-10-21 18:42:14 +0800

    management page

---
+++ back/category.php
@@ -0,0 +1,81 @@
+<?php
+    require("../state.php");
+    require("../model/sql.php");
+
+    $sql = new MySql();
+
+    $result = $sql->select("id, category, iframesrc", "category")->query();
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>backend</title>
+    <link rel="stylesheet" href="../lib/bootstrap/css/bootstrap.min.css">
+    <script src="../lib/jquery-2.0.1.min.js"></script>
+    <script src="../lib/bootstrap/js/bootstrap.min.js"></script>
+    <style>
+      .table .table-ifrsrc{
+        overflow: hidden;
+        text-overflow: ellipsis;
+        /* width: 500px; */
+        word-break: break-all;
+        white-space: nowrap;
+        width: 70%;
+      }
+    </style>
+</head>
+<body>
+  <div class="container">
+      <nav class="navbar navbar-default" role="navigation">
+        <div class="container-fluid">
+          <div class="navbar-header">
+            <a class="navbar-brand" href="#">金分</a>
+          </div>
+          <ul class="nav navbar-nav navbar-right">
+            <li><a href="#"><?php echo $_SESSION["email"] ?></a></li>
+            <li><a href="./loginout.php">退出</a></li>
+          </ul>
+        </div>
+      </nav>
+    <div class="row">
+        <div class="col-md-3">
+          <ul class="list-group">
+            <li class="list-group-item"><a href="./category.php">添加类别</a></li>
+          </ul>
+        </div>
+        <div class="col-md-9">
+          <div class="panel panel-default">
+            <div class="panel-body">
+
+      			<table class="table table-striped">
+              <?php $index = 0 ?>
+              <?php foreach ($result as $value) {?>
+      				<tr>
+                <td><?php echo ++$index ?></td>
+                <td><?php echo $value['category']?></td>
+                <td class="table-ifrsrc" title="<?php echo $value['iframesrc']?>"><div style="width: 500px"><?php echo $value['iframesrc']?></div></td>
+              </tr>
+              <?php }?>
+      			</table>
+
+      			<form action="./cateHandle.php" method="POST">
+      				<div class="form-group">
+      					<label for="category">类别</label>
+      					<input type="text" class="form-control" name="category" id="category" placeholder="category">
+      				</div>
+      				<div class="form-group">
+      					<label for="iframesrc">iframe 链接</label>
+      					<textarea class="form-control" name="iframesrc" id="iframesrc" placeholder="iframe src"></textarea>
+      				</div>
+      				<button type="submit" class="btn btn-primary">提交</button>
+      			</form>
+            </div>
+          </div>
+        </div>
+    </div>
+  </div>
+</body>
+</html>
---
+++ model/sql.php
@@ -0,0 +1,137 @@
+<?php
+
+class MySql{
+	
+	//链接状态
+	private $_connection;
+	//查询得到的sql，用于链式存储
+	private $_sql;
+	
+	function __construct(){
+		$this->connect();
+	}
+	
+	//链接数据库
+	function connect()
+	{
+		//1 首先链接数据
+		$con = mysql_connect("127.0.0.1:3306", "root", "");
+		
+		//链接的时候用UTF8 字符集通信
+		mysql_query("SET NAMES 'utf8'");
+
+		if (!$con) {
+			die("链接不成功");
+		}
+		
+		// 2 选择要操作的数据
+		mysql_select_db("younglogin", $con);
+		
+		$this->_connection = $con;
+	}
+	
+	/**
+	* 查询
+	* $target 查询对象，用逗号符分割
+	* $table 查询的表
+	*/
+	function select($target, $table)
+	{
+		$this->_sql = "select ". $target ." from ". $table;
+		return $this;
+	}
+
+	/**
+	* 查询
+	* @param string $key 查询key
+	* @param string $value 查询value
+	* @param string $operator 操作符
+	* @return object
+	*/
+	function where($key, $value, $operator="="){
+		if(!strpos($this->_sql, "where")){
+			$this->_sql = $this->_sql." where ".$key.$operator."'".$value."'";
+		}else{
+			$this->_sql = $this->_sql." and ".$key.$operator."'".$value."'";
+		}
+
+		return $this;
+	}
+	
+	function limit($num){
+		$this->_sql = $this->_sql." limit ".$num;
+		
+		return $this;
+	}
+	
+	function query(){
+		// 3 执行要做得sql 
+		
+		$exe = mysql_query($this->_sql);
+
+		if (strpos($this->_sql, "select") === 0) {
+			//如果是select
+
+			//mysql_fetch_assoc 仅仅输出一条数据，所以需要循环出来
+			// 4 获取返回的结果
+			// 定义$result
+			$result = array();
+			
+			while($data = mysql_fetch_assoc($exe))
+			{
+				$result[] = $data;
+			}
+			
+			return $result;
+
+		}else{
+
+			//如果是其他
+			return $exe;
+
+		}
+		
+
+	}
+	
+	/*
+	* 插入
+	*/
+	function insert($table, $arr)
+	{	
+		$strKeys = "";
+		$strValues = "";
+		
+		foreach($arr as $key=>$value){
+			$strKeys .= ",".$key;
+			//addslashes 将单引号转义
+			$strValues .= ","."'".addslashes($value)."'";
+		}
+		
+		//过滤左边逗号
+		$strKeys = ltrim($strKeys, ',');
+		$strValues = ltrim($strValues, ',');
+	
+		$this->_sql = "insert into ".$table." (".$strKeys.") values(".$strValues.")";
+
+		//$result = mysql_query($sql);
+		
+		return $this;
+	}
+
+	/**
+	 * 删除
+	 */
+	function delete($table)
+	{
+
+	}
+	
+	function __destruct(){
+		// 5 关闭数据库
+		mysql_close($this->_connection);
+	}
+}
+
+
+?>
