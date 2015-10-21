da5f44ffe2ac180ac20ca0ceea74ec9fe46dd809
Young Foo <fuymsn@hotmail.com>
2015-10-21 10:52:16 +0800

    change sql

--- younglogin.sql
+++ younglogin.sql
@@ -1,32 +1,105 @@
-/*
-Navicat MySQL Data Transfer
+-- phpMyAdmin SQL Dump
+-- version 4.4.14
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: Oct 21, 2015 at 04:49 AM
+-- Server version: 5.6.26
+-- PHP Version: 5.6.12
 
-Source Server         : 10.1.10.145
-Source Server Version : 50622
-Source Host           : localhost:3366
-Source Database       : younglogin
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
 
-Target Server Type    : MYSQL
-Target Server Version : 50622
-File Encoding         : 65001
 
-Date: 2015-10-16 18:32:09
-*/
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
-SET FOREIGN_KEY_CHECKS=0;
+--
+-- Database: `younglogin`
+--
 
--- ----------------------------
--- Table structure for user
--- ----------------------------
-DROP TABLE IF EXISTS `user`;
-CREATE TABLE `user` (
-  `uid` int(11) NOT NULL AUTO_INCREMENT,
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `category`
+--
+
+CREATE TABLE IF NOT EXISTS `category` (
+  `uid` int(11) NOT NULL,
+  `category` varchar(32) NOT NULL,
+  `iframesrc` varchar(32) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='添加金分类别';
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE IF NOT EXISTS `user` (
+  `uid` int(11) NOT NULL,
   `email` varchar(32) NOT NULL,
-  `pwd` varchar(32) NOT NULL,
-  PRIMARY KEY (`uid`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
+  `pwd` varchar(32) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=26 DEFAULT CHARSET=utf8;
 
--- ----------------------------
--- Records of user
--- ----------------------------
-INSERT INTO `user` VALUES ('1', 'fuymsn@hotmail.com', '111111');
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`uid`, `email`, `pwd`) VALUES
+(1, 'fuymsn@hotmail.com', '111111'),
+(7, 'test@test.com', '111111'),
+(8, 'young@qq.com', '111111'),
+(9, 'young@qq1.com', '111111'),
+(10, 'young1@qq.com', '111111'),
+(11, 'young2@qq.com', '111111'),
+(12, 'young3@qq.com', '111111'),
+(13, 'young4@qq.com', '111111'),
+(14, 'fuymsn1@hotmail.com', '111111'),
+(15, 'fuymsn2@hotmail.com', '111'),
+(16, 'fuymsn4@hotmail.com', '111111'),
+(17, 'fuymsn5@hotmail.com', '111111'),
+(18, 'test1@test.com', '111'),
+(19, 'fuymsn@hotmail.com77', '1111111111'),
+(20, 'fuymsn@hotssmail.com', '111111'),
+(21, 'fuymsn@hotmail.cosss', '111111'),
+(22, '111111@qqc.p', '111111'),
+(23, 'fuymsn9@hotmail.com', '111111'),
+(24, 'sss@qqq.com', '111111'),
+(25, 'admin@admin.com', '123456');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `category`
+--
+ALTER TABLE `category`
+  ADD PRIMARY KEY (`uid`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`uid`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `category`
+--
+ALTER TABLE `category`
+  MODIFY `uid` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `uid` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=26;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
