4fbb61f1898a100921bd2159dc9b1926593951b6
Young Foo <fuymsn@hotmail.com>
2015-10-23 00:21:03 +0800

    route

--- core/route.php
+++ core/route.php
@@ -1,13 +1,29 @@
 <?php
 
 require('./controller/BaseController.php');
-
+//
 $path = isset($_SERVER['PATH_INFO']) ? $_SERVER['PATH_INFO'] : ""; //path
 
+//
 $pathArr = explode("/", $path);
 $pathArrLen = count($pathArr);
 
-function router($handle, $pathArr)
+$route = array(
+	'home/index'=>array('GET,POST','HomeController@index'),
+	'home/login'=>'HomeController@login',
+	'u/{id:\d}'=>'MemberController@index',
+	'page[/{name:\w}]'=>'PageController@index',
+);
+
+$router = new Router($route);
+$result = $router->pase($path);
+
+
+$dispather = new Dispather();
+$dispather->do($result);
+
+//分发器，调度器
+function dispather($handle, $pathArr)
 {
 	$controller = ucfirst($pathArr[1] . 'Controller'); //controller name
 	require('./controller/'.$controller.'.php');
@@ -24,12 +40,12 @@ if ($pathArrLen == 1) {
 
 //如果只有一级路由
 }else if($pathArrLen == 2){
-	router("index", $pathArr);
+	dispather("index", $pathArr);
 
 //如果只有二级路由
 }else if($pathArrLen == 3) {
 	
-	router($pathArr[2], $pathArr);
+	dispather($pathArr[2], $pathArr);
 
 }
 
